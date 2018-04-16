0b59c91fe2c5ef3a40156dbcc318d93d1f0cc2af
subin <subibi21@naver.com>
2018-04-16 19:12:31 +0900

    Delete clova.js

--- routes/clova.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const cekRequest = require('../clova');
-const { json } = require('../http');
-const router = express.Router();
-
-router.post('/', json(cekRequest));
-
-module.exports = router;
